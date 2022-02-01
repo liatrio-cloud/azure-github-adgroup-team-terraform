d37ebfbb720ab52a725bebc5d9a7ee30761b6090
Steven O <steveno@liatrio.com>
2022-02-01 11:07:21 -0600

    updated backend

--- tf/backend.tf
+++ tf/backend.tf
@@ -1,8 +1,3 @@
 terraform {
-  backend "azurerm" {
-    storage_account_name = "[AZURE_STORAGE_ACCOUNT]"
-    resource_group_name  = "[AZURE_RESOUCE_GROUP]"
-    container_name       = "[TERRAFORM_STATE_CONTAINER_NAME]"
-    key                  = "[TERRAFORM_STATE_FILE_KEY]"
-  }
+  backend "azurerm" {}
 }
