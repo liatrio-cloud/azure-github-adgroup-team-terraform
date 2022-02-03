1c0da12389785000b35de3a208550d1333fa9945
Steven O <steveno@liatrio.com>
2022-02-03 16:36:46 -0600

    cleanup

--- module-team-config/repos.tf
+++ module-team-config/repos.tf
@@ -1,13 +1,5 @@
-# Uncomment the below to pull in repos based on name.
-# We're using a GitHub app to manage protections, so this conflicts
-
-#data "github_repositories" "team_repos" {
-#  query = "org:${var.org} ${var.team_name}-"
-#}
-
 locals {
   team_repo_list = concat(
-    #    data.github_repositories.team_repos.names,
     var.team_repos
   )
 }
@@ -19,15 +11,3 @@ resource "github_team_repository" "team_repo_link" {
   repository = each.value
   permission = var.permission
 }
-
-# Uncomment the below to apply branch protections to the team's repositories
-# We're using a GitHub app to manage protections, so this conflicts
-
-#module "branch_protections" {
-#  source = "../module-repo-config"
-#  providers = {
-#    github = github
-#  }
-#
-#  repos = local.team_repo_list
-#}
